54819d1a32cf5d2670c0e4f58070daaa56788afc
waqas <waqasahsan84@gmail.com>
2024-02-03 01:26:37 +0500

    conflict resolved

--- 03_gpts/01_location_action/test_location.py
+++ 03_gpts/01_location_action/test_location.py
@@ -9,11 +9,14 @@ locations = {
     "javed": Location(name="Javed", location="Lahore"),
 }
 
-def fake_get_location_or_404(name:str)->Location:
+
+def fake_get_location_or_404(name: str) -> Location:
     loc = locations.get(name.lower())
     if not loc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No location found for {name}")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"No location found for {name}")
     return loc
+
 
 # Comment out this line to stop overriding the dependency
 app.dependency_overrides[get_location_or_404] = fake_get_location_or_404
@@ -26,7 +29,7 @@ def test_read_location():
     assert response.status_code == 200
     assert response.json() == {"name": "Zeeshan", "location": "Karachi"}
 
+
 def test_location_404():
     response = client.get("/location/john")
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    
