d15a95e7a57403e272a4c79c9f201ef67402c4db
waqas <waqasahsan84@gmail.com>
2024-02-03 01:24:29 +0500

    Crud complete added for model.py

--- 03_gpts/01_location_action/location.py
+++ 03_gpts/01_location_action/location.py
@@ -3,18 +3,20 @@ from pydantic import BaseModel
 from typing import Annotated
 
 app = FastAPI(
-    title="Location Finder API", 
+    title="Location Finder API",
     version="1.0.0",
     servers=[
         {
-            "url": "http://localhost:8000", # ADD NGROK URL Here Before Creating GPT Action
+            "url": "http://localhost:8000",  # ADD NGROK URL Here Before Creating GPT Action
             "description": "Development Server"
         }
-        ])
+    ])
+
 
 class Location(BaseModel):
     name: str
     location: str
+
 
 locations = {
     "zia": Location(name="Zia", location="Karachi"),
@@ -22,14 +24,16 @@ locations = {
 }
 
 # dependency function
-def get_location_or_404(name:str)->Location:
+
+
+def get_location_or_404(name: str) -> Location:
     loc = locations.get(name.lower())
     if not loc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No location found for {name}")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"No location found for {name}")
     return loc
 
 
 @app.get("/location/{name}")
-def get_person_location(name:str, location: Annotated[Location, Depends(get_location_or_404)]):
+def get_person_location(name: str, location: Annotated[Location, Depends(get_location_or_404)]):
     return location
-
--- 03_gpts/02_sql_model/model.py
+++ 03_gpts/02_sql_model/model.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select, delete
+
+# https://sqlmodel.tiangolo.com/tutorial/delete/
 
 
 class Hero(SQLModel, table=True):
@@ -10,18 +12,81 @@ class Hero(SQLModel, table=True):
     age: Optional[int] = None
 
 
-hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+engine = create_engine(
+    "postgresql://hawkhere:8cxCnEDsYj1q@ep-dark-math-37799263.us-east-2.aws.neon.tech/neondb?sslmode=require", echo=True)
 
 
-engine = create_engine("postgresql://ziaukhan:Lz9i@ep-little-frog-313391.us-east-2.aws.neon.tech/neondb?sslmode=require")
+def create_database_and_tables():
+    """
+    Creates a database by creating all tables defined in the SQLModel metadata using the provided engine.
+    """
+    SQLModel.metadata.create_all(engine)
 
 
-SQLModel.metadata.create_all(engine)
+def add_data():
+    """
+    A function to add data to the database using a session object.
+    """
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        session.commit()
 
-with Session(engine) as session:
-    session.add(hero_1)
-    session.add(hero_2)
-    session.add(hero_3)
-    session.commit()
+# get data from the database
+
+
+def get_all_data():
+    """
+    A function to retrieve all data from the database using the Hero table.
+    """
+    with Session(engine) as session:
+        statement = select(Hero)
+        results = session.exec(statement)
+        for result in results:
+            print(result)
+
+
+def get_data():
+    """
+    This function retrieves data using a database session and prints the results.
+    """
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.age == None)
+        results = session.exec(statement)
+        for result in results:
+            print(result)
+
+#  update data
+
+
+def update_data():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.age == 6)
+        results = session.exec(statement)
+        for result in results:
+            result.age = 7
+            session.commit()
+            print(result)
+
+#  delete data
+
+
+def delete_data():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.age == None)
+        result = session.exec(statement)
+        result = result.one()
+        session.delete(result)
+        session.commit()
+        print(f"Delete Object : {result}")
+
+
+if __name__ == "__main__":
+    # get_all_data()
+    # get_data()
+    # update_data()
+    delete_data()
