fc88bad56ce9b98a95b1e71148e92441f2a72ad5
waqas <waqasahsan84@gmail.com>
2024-02-07 12:32:11 +0500

    sqlmodel implementation in progress

--- 03_gpts/03_simple_hero_api/main.py
+++ 03_gpts/03_simple_hero_api/main.py
@@ -2,6 +2,12 @@ from typing import Optional
 
 from fastapi import FastAPI
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+# is it from online class 5 at 31 Jan 2024?
+# now adding more features in it.
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
+
+load_dotenv(find_dotenv())
 
 
 class Hero(SQLModel, table=True):
@@ -9,12 +15,25 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = Field(default=None, index=True)
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+
+# create a team table with fields id, name and headquarters.
 
 
-database_url = "postgresql://ziaukhan:oSLz91i@ep-little-frog-313391.us-east-2.aws.neon.tech/neondb?sslmode=require"
+class Team(SQLModel, table=True):
+    '''
+    created a team table with fields id, name and headquarters.
+    '''
+    id: Optional[int] = Field(primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+
+
+# database_url = "postgresql://ziaukhan:oSLz91i@ep-little-frog-313391.us-east-2.aws.neon.tech/neondb?sslmode=require"
+database_url = getenv("DATABASE_URL")
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(database_url)
+engine = create_engine(database_url, echo=True)
 # engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 
@@ -44,3 +63,7 @@ def read_heroes():
     with Session(engine) as session:
         heroes = session.exec(select(Hero)).all()
         return heroes
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
